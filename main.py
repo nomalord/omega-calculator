c9260ccd29e32e286ad873c4fca7a7300da27843
noam koushnir <nkoushnir@gmail.com>
2022-11-14 17:13:52 +0200

    commit of operations

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
-num = 2
+operator_dict = {'+':1, '-':1, '*':2, '/':2, '^':3, '%':4,
+                 '$':5, '&':5, '@':5, '~':6, '!':6, '(':7, ')':7}
 
-print(num)
+def add(num1, num2):
+    return num1 + num2
 
-def check(num):
-    num += 1
-    return num
+def subtract(num1, num2):
+    return num1 - num2
 
-num = check(num)
+def multiply(num1, num2):
+    return num1 * num2
 
-print(num)
+def divide(num1, num2):
+    return num1 / num2
+
+def power(num1, num2):
+    return num1 ** num2
+
+def modulus(num1, num2):
+    return num1 % num2
+
+def maximum(num1, num2):
+    """Returns the maximum of two numbers ($)."""
+    if(num1 > num2):
+        return num1
+    else:
+        return num2
+
+def minimum(num1, num2):
+    """Returns the minimum of two numbers (&)."""
+    if(num1 < num2):
+        return num1
+    else:
+        return num2
+
+def average(num1, num2):
+    """Returns the average of two numbers (@)."""
+    return (num1 + num2) / 2
+
+def factorial(num):
+    if(num == 0):
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+def absolute(num):
+    """Returns the negative value of a number (~)."""
+    num*-1
